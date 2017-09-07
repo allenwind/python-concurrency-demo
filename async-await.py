1b19e6de1ed0662287cd6f5fcca2eb3ab0a35931
allenwind <allenwind@foxmail.com>
2017-09-07 22:32:50 +0800

    async-wait

--- async-await.py
+++ async-await.py
@@ -1,19 +1,20 @@
 import asyncio
 
-async def printer():
+async def print_a_c():
     print("A")
     await asyncio.sleep(0)
     print("C")
 
-async def printer2():
+async def print_b_d():
     print("B")
     await asyncio.sleep(0)
     print("D")
 
 async def main():
-    await asyncio.wait([printer2(), printer()])
+    await asyncio.wait([print_b_d(), print_a_c()])
 
-loop = asyncio.get_event_loop()
-loop.run_until_complete(main())
+if __name__ == '__main__':
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(main())
 
 
